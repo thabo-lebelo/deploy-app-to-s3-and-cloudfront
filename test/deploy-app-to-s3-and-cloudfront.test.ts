2cc4e8cca343eed47b64346ecabcb012b09b4e2f
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-18 11:31:07 +0200

    Initializing a CDK project

---
+++ test/deploy-app-to-s3-and-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DeployAppToS3AndCloudfront from '../lib/deploy-app-to-s3-and-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deploy-app-to-s3-and-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DeployAppToS3AndCloudfront.DeployAppToS3AndCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
